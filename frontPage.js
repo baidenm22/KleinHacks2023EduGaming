a03bc68b833886498501a384be8178c73caf4a20
Aiden McBride <aidenmcbride@live.com>
2023-04-01 16:30:12 -0500

    FInishing Up

--- frontPage.js
+++ frontPage.js
@@ -12,3 +12,13 @@ function randomLocation()
     this.style.top = Math.random() * 100 + '%';
     this.style.left = 0;
 }
+
+document.getElementById("answerButton").onclick = function() {
+
+    var answer = document.getElementById("answerSpace").value;
+    var answers = [20,30,40,50]
+    if(answer = answers[0])
+    {
+     console.log("success");   
+    }
+}
