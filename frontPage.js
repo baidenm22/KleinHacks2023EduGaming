32c9b563ba5901211bfae3cb2f3841d5d7867d6c
Aiden McBride <aidenmcbride@live.com>
2023-04-01 14:57:39 -0500

    paticles

--- frontPage.js
+++ frontPage.js
@@ -1,14 +1,16 @@
+for(let i = 0; i < 100; i++)
+
 {
     const sand = document.createElement('div');
     sand.className = 'sand';
-    sand.style.left = Math.random() * 100 + '%';
+    sand.style.left = 0;
     sand.style.animationDelay = Math.random()*10 +'s';
     sand.addEventListener('animationiteration', randomLocation);
     document.getElementById('sands').append(sand);
 }
 function randomLocation()
 {
-    this.style.top = 0;
-    this.style.left = Math.random() * 100 + '%';
+    this.style.top = Math.random() * 100 + '%';
+    this.style.left = 0;
     this.style.borderColor = 'rgba(38, 38, 148,'+ Math.random() + ')';
 }
